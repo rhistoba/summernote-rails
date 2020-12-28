13417f50d27df7a3ce06a6630a5d57f3c7e7d354
rhistoba <fujitakeisound@gmail.com>
2020-12-28 14:15:21 +0900

    HTMLタグのフィルター、iframeのドメインのフィルターの設定を変更

--- app/assets/javascripts/summernote-init.js
+++ app/assets/javascripts/summernote-init.js
@@ -1,5 +1,10 @@
 $(document).on('turbolinks:load', () => {
   $('[data-provider="summernote"]').each((index, element) => {
-    $(element).summernote({height: 600, lang: 'ja-JP'})
+    $(element).summernote({
+      height: 600,
+      lang: 'ja-JP',
+      codeviewFilter: true,
+      codeviewIframeFilter: false,
+    })
   })
 })
